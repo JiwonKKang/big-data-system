9dac12ccc7473e7d886954712b0e63c6ee8f5641
강지원 <kangji149@naver.com>
2024-06-10 14:30:42 +0900

    a

---
+++ src/main/java/com/bigdata/library/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.bigdata.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("DELETE", "GET", "POST", "PATCH", "PUT")
+                .allowedHeaders("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization")
+                .exposedHeaders("*")
+                .maxAge(3600);
+    }
+}
